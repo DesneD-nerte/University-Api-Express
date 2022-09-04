cfd240e70bce97f9ce7048ab7c3e46c85c821a34
Alexandr Ershov <ershov.avdd@gmail.com>
2022-09-04 21:26:00 +0700

    Configured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,40 @@
-import path from "path";
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const outputPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
-	mode: "development",
+	mode: "production",
 	entry: "./src/app.ts",
 	output: {
-		fileName: "app.ts",
-		path: path.resolve(__dirname, "dist"),
+		filename: "app.bundle.js",
+		path: outputPath
 	},
 	resolve: {
-		extenstions: [".ts", ".js"]
+		extensions: [".ts", ".js"]
 	},
 	module: {
 		rules: [
-			{ test: /\.ts$/, loader: "ts-loader"}
+			{ test: /\.ts$/, loader: "ts-loader", exclude: "/node_modules"}
 		]
-	}
+	},
+	node: {
+		__dirname: true
+	},
+	plugins: [
+		new CopyWebpackPlugin({
+			patterns: [
+				'./node_modules/swagger-ui-dist/swagger-ui.css',
+				'./node_modules/swagger-ui-dist/swagger-ui-bundle.js',
+				'./node_modules/swagger-ui-dist/swagger-ui-standalone-preset.js',
+				'./node_modules/swagger-ui-dist/favicon-16x16.png',
+				'./node_modules/swagger-ui-dist/favicon-32x32.png',
+
+				{from: "./src/files", to: "files"},
+				{from: "./src/images", to: "images"},
+			]
+		}),
+	],
+	target: "node"
 }
