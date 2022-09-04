cfced2e0f9d95cba2f82a2be824aa4b57ba5881a
Alexandr Ershov <ershov.avdd@gmail.com>
2022-09-04 17:06:30 +0700

    Removed dist folder

--- dist/models/News.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const News = new mongoose_1.Schema({
-    name: { type: String, required: true },
-    content: { type: String, required: true },
-    createdAt: { type: Date, required: true }
-});
-exports.default = (0, mongoose_1.model)("News", News);
